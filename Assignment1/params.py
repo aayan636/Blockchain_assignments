b682cca6c2560d61901df39366b9adf54060620c
Aayan Kumar <aayan.kumar123@gmail.com>
2018-02-10 13:32:02 +0530

    Peer main thread added

--- Assignment1/params.py
+++ Assignment1/params.py
@@ -2,3 +2,4 @@ class Parameters:
   """All the required parameters"""
   num_peers = 10
   num_slow_nodes = 10
+  transaction_generation_mean_time = 1
--- Assignment1/peer.py
+++ Assignment1/peer.py
@@ -1,12 +1,38 @@
-class Peer:
+import threading
+import time
+#
+import numpy as np
+#
+from params import Parameters
+
+class Peer (threading.Thread):
   """Peer class"""
   # pid
   # is_slow
   # gen_transaction()
   
-  def __init__(self):
-    pass
+  def __init__(self, pid, is_slow):
+    threading.Thread.__init__(self)
+    self.pid = pid
+    self.is_slow = is_slow
+
+  def gen_transaction(self):
+    while True:
+      waiting_time = np.random.exponential(Parameters.transaction_generation_mean_time)
+      time.sleep(waiting_time)
+      print "Txn generated ", self.pid
+
+  def run(self):
+    print "Starting Peer ", self.pid
+    self.gen_transaction()
+    print "Exiting Peer ", self.pid
+
 
 # For testing
 if __name__ == '__main__':
-  pass
+  a = Peer(1, False)
+  a.start()
+  b = Peer(2, False)
+  b.start()
+  c = Peer(3, False)
+  c.start()
