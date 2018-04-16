b4e85114c02a4e922ca48aeb2f031ccbb269f84c
Aditi <cs1140205@iitd.ac.in>
2018-04-16 22:02:31 +0530

    UPdated test.js

--- Assignment2/test.js
+++ Assignment2/test.js
@@ -21,11 +21,13 @@ deployedContract = MainContract.new({data: byteCode, from: web3.eth.accounts[0],
       acct_address = web3.eth.accounts[0]
       contractInstance.make_creator({data: byteCode, from: acct_address, gas: 4700000})
       console.log("Testing get_all_creators", contractInstance.get_all_creators({data: byteCode, from: acct_address, gas: 4700000}))
-      contractInstance.add_media(5, 4, [web3.eth.accounts[9], web3.eth.accounts[7]], [50, 30], {data: byteCode, from: acct_address, gas: 4700000})
+      contractInstance.add_media(5, 4, [web3.eth.accounts[9]], [50], {data: byteCode, from: acct_address, gas: 4700000})
       media_list = contractInstance.get_all_media(true, {data: byteCode, from: web3.eth.accounts[5], gas: 4700000})
       console.log("Testing get_all_media", media_list)
       media_id = 0
-      console.log("Testing buy media", contractInstance.buy_media(media_list[0][media_id], media_list[1][media_id], true, {data: byteCode, from: web3.eth.accounts[5], gas: 4700000}))
+      wei_value = web3.toWei(media_list[2][media_id], 'ether')
+      console.log("Value in wei : ", wei_value)
+      console.log("Testing buy media", contractInstance.buy_media(media_list[0][media_id], media_list[1][media_id], true, {data: byteCode, from: web3.eth.accounts[5], gas: 4700000, value: wei_value}))
     }
   }
 )
